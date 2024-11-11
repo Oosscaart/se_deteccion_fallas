01422b4c71ed77d8bf995f63d507f8901cf94cf3
Oosscaart <91275922+Oosscaart@users.noreply.github.com>
2024-11-10 23:39:05 -0600

    Agregacion columna 'Bandera' y 'suma' en la bd

---
+++ queries/alter_queries.sql
@@ -0,0 +1,10 @@
+----------------------------------------- MODIFICACION DE TABLAS ------------------------------------------
+-- Modificacion tabla fallas_computadora
+ALTER TABLE fallas_computadora
+ADD COLUMN suma  INT;
+
+ALTER TABLE caracteristicas
+ADD COLUMN bandera  BOOLEAN;
+
+------------------------------------------------------------------------------------------------------------
+
